256d3369e85ed8b1f5987046c9e48c0ed54c82bc
Jacob Ferrante <jacobferrante@protonmail.com>
2022-11-16 09:36:37 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'admin.bikemgtwn.com',
-        port: '',
-        pathname: '/assets/**',
-      }
-    ]
-   }
 }
 
 module.exports = nextConfig
