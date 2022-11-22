bb7d01a14b9ac99667cd0ed7c189e707ba71c031
Jacob Ferrante <jacobferrante@protonmail.com>
2022-11-22 00:37:04 -0500

    messing with things, hoping this builds correctly, having issues on local

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,14 @@
 import Layout from '../components/Layout'
 import Script from 'next/script'
 import '../styles/globals.css'
-import { Roboto_Flex } from '@next/font/google'
 
-const robotoFlex = Roboto_Flex()
 
 function MyApp({ Component, pageProps }) {
-  return (
-  
-  <main className={robotoFlex.className}>
+  return ( 
   <Layout>
     <Script src="https://kit.fontawesome.com/9897761609.js" crossorigin="anonymous" />
     <Component {...pageProps} />
   </Layout>
-  </main>
   )
 }
 
